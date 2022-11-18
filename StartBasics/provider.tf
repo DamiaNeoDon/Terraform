369fcf778754483324c1867cf4c09b37f64356eb
DamiaNeoDon <83450931+DamiaNeoDon@users.noreply.github.com>
2022-11-18 12:13:48 +0100

    Add files via upload

--- StartBasics/provider.tf
+++ StartBasics/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   access_key = "AKIARFRLO2NV5DCJANRD"
-  secret_key = "a1Ypl47Gy9aacI19jEwM4k8fhYm4+n7RQOJRnSV"
+  secret_key = "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"
   region     = "us-east-1"
 }
