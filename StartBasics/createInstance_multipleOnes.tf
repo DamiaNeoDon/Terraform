1f78fe3d414b6dff6192ffce16c573d970a8d48d
DamiaNeoDon <83450931+DamiaNeoDon@users.noreply.github.com>
2022-11-19 12:17:23 +0100

    Add files via upload

--- StartBasics/createInstance_multipleOnes.tf
+++ StartBasics/createInstance_multipleOnes.tf
@@ -1,10 +1,10 @@
 
-resource "aws_instance" "MojPierwszyVar" {
+resource "aws_instance" "MojPierwszyVar2" {
   count         = 3
   ami           = "ami-07957d39ebba800d5"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "demoinstnce-${count.index}"
+    Name = "demoinstnce-${count.index}" #renamuje count instances by się różniły w awsie
   }
 }
